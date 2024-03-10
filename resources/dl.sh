28a079c4ca2581a92e11bf6cbbb94ac8f9c183b5
Paul Baecher <pbaecher@gmail.com>
2024-03-10 21:05:55 +0100

    Revise download script

--- resources/dl.sh
+++ resources/dl.sh
@@ -2,35 +2,67 @@
 
 set -eo pipefail
 
-if [ $# -ne 2 ]
+if [ $# -ne 1 ] && [ $# -ne 2 ]
 then
-    echo "usage: $0 URL OUTPUT_PATH"
+    echo "usage: $0 URL [OUTPUT_DIR]"
     exit 1
 fi
 
 URL="$1"
-OUTPUT_PATH="$2"
+OUTPUT_PATH="${2-.}"
 
-if [[ "$URL" =~ soundcloud\.com/[^/]*/sets ]]
-then
+if [[ "$URL" =~ soundcloud\.com/[^/]*/sets ]]; then
     >&2 echo "$0: downloading entire playlists is not supported yet"
     exit 1
 fi
 
-WORKDIR=$(mktemp -d)
-trap "rm -rf $WORKDIR" EXIT
+WORKDIR_LINK="${TMPDIR:-/tmp}/ytdl-workdir"
 
-pushd "$WORKDIR" > /dev/null
+download() {
+    REUSE_WORKDIR="$1"
 
-echo "Timestamp: $(date -Is)"
-python3 -m venv e
-e/bin/pip --no-color --no-cache-dir --disable-pip-version-check install --progress-bar=off yt-dlp
-e/bin/yt-dlp --flat-playlist -x --audio-quality=0 --no-simulate -O 'id: %(id)s' -O 'title: %(title)s' -O 'original_url: %(original_url)s' --exec echo --newline --no-colors "$URL" | tee log
+    if [ "$REUSE_WORKDIR" -eq 1 ] && [ -d "$WORKDIR_LINK" ]; then
+        echo "reusing existing workdir"
+        pushd "$WORKDIR_LINK" > /dev/null
+    else
+        echo "using a fresh workdir"
 
-OUTFILE=$(tail -n 1 log)
+        if [ ! -z "$(readlink $WORKDIR_LINK)" ]; then
+            rm -rf "$(readlink $WORKDIR_LINK)"
+        fi
 
-popd > /dev/null
+        rm -f "$WORKDIR_LINK"
+        ln -s "$(mktemp --directory --tmpdir ytdl-workdir-XXXXXXX)" "$WORKDIR_LINK"
 
-mv "$OUTFILE" "$OUTPUT_PATH"
+        pushd "$WORKDIR_LINK" > /dev/null
 
-basename "$OUTFILE"
+        python3 -m venv e
+        e/bin/pip --no-color --no-cache-dir --disable-pip-version-check install --progress-bar=off yt-dlp
+
+        REUSE_WORKDIR=0
+    fi
+
+    echo "timestamp is $(date -Is)"
+    if e/bin/yt-dlp --flat-playlist -x --audio-quality=0 --no-simulate -O 'id: %(id)s' -O 'title: %(title)s' -O 'original_url: %(original_url)s' --exec echo --newline --no-colors "$URL" | tee log; then
+        echo "yt-dlp finished successfully"
+    else
+        if [ "$REUSE_WORKDIR" -eq 1 ]; then
+            echo "yt-dlp failed, retrying with a fresh Python env"
+            popd > /dev/null
+            download 0
+        else
+            echo "yt-dlp failed unrecoverably"
+            exit 1
+        fi
+    fi
+
+    OUTFILE=$(tail -n 1 log)
+
+    popd > /dev/null
+
+    mv "$OUTFILE" "$OUTPUT_PATH"
+
+    basename "$OUTFILE"
+}
+
+download 1
